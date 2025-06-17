1696ca72c4cfebfb34157598e6a0f6da5eaba429
osama <obg1309@gmail.com>
2025-06-17 11:42:15 +0200

    exercise finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css'
+import { useState } from 'react'
+
 
 function App() {
- //Aquí tu código
+    const [name, setName] = useState('Sofia')
+    const [newName, setNewName] = useState('')
+
+    return (
+        <div>
+        <h2>Teacher Name: {name}</h2>
+        <ul>
+            <li onClick={() => setName('Data')}>Data</li>
+            <li onClick={() => setName('Reyes')}>Reyes</li>
+            <li onClick={() => setName('Yolanda')}>Yolanda</li>
+        </ul>
+        </div>
+    );
 }
 
 export default App
